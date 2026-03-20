95f63a9cec82f9b492e34e6ca94d67bf0e5de12d
Kalyani <kolekargauri8@gmail.com>
2026-03-20 18:20:16 +0530

    Fix mobile preloader caching issue
    - Add cache-busting query parameter (?v=20260320) to preloader logo
    - Update both Preloader.jsx and loading.js components
    - Force mobile browsers to load new preloader logo instead of cached version

--- mbsm/src/app/loading.js
+++ mbsm/src/app/loading.js
@@ -2,7 +2,7 @@
 export default function Loading() {
   return (
     <div className='preloader'>
-      <img src='/assets/images/preloader-logo.png' alt='MBSM Trust Logo' />
+      <img src='/assets/images/preloader-logo.png?v=20260320' alt='MBSM Trust Logo' />
       <p>MBSM Trust</p>
     </div>
   );
--- mbsm/src/components/Preloader.jsx
+++ mbsm/src/components/Preloader.jsx
@@ -12,7 +12,7 @@ const Preloader = () => {
     <>
       {active ? (
         <div className='preloader'>
-          <img src='/assets/images/preloader-logo.png' alt='MBSM Trust Logo' />
+          <img src='/assets/images/preloader-logo.png?v=20260320' alt='MBSM Trust Logo' />
           <p>MBSM Trust</p>
         </div>
       ) : (
